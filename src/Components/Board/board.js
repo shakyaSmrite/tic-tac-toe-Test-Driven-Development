abc0b9d5dd5aeb4c434785aab16ab5981789f7e5
Smriti Shakya <smriti.shakya@mavs.uta.edu>
2021-11-14 19:16:25 -0600

    onclick check

--- src/Components/Board/board.js
+++ src/Components/Board/board.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Square from "./../Square/square";
+// import Square from "./../Square/square";
 
 function Board() {
   return (
--- src/Components/Board/board.test.js
+++ src/Components/Board/board.test.js
@@ -5,6 +5,15 @@ import { shallow } from "enzyme";
 describe("Board", () => {
   //it global functions by jest
   it("Renders successfully without crashing", () => {
-    shallow(<Board />);
+    let squares = Array(9).fill(null);
+    shallow(<Board square={squares} />);
+  });
+
+  it("calls onClick event on click of a square in board", () => {
+    let squares = Array(9).fill(null);
+    const onClick = jest.fn();
+    let wrapper = mount(<Board squares={squares} onClick={onClick} />);
+    wrapper.find("button.square").first().simulate("click");
+    expect(onClick).toBeCalledWith(0);
   });
 });
